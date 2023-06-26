1e5a3bbc5e49bf1eeeadd200b0cc39d87adbc44d
FoxA23 <afoxaa96@gmail.com>
2023-06-26 04:18:15 +0300

    Редактирование отступов во второй задачи

--- Homework_1/Task_2.java
+++ Homework_1/Task_2.java
@@ -4,47 +4,46 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Task_2 {
-    public static void main(String[] args) {
-        int[] array1 = generateRandomArray();
-        int[] array2 = generateRandomArray();
+	public static void main(String[] args) {
+		int[] array1 = generateRandomArray();
+		int[] array2 = generateRandomArray();
 
-        System.out.println("\nМассив 1: " + Arrays.toString(array1));
-        System.out.println("Массив 2: " + Arrays.toString(array2));
+		System.out.println("\nМассив 1: " + Arrays.toString(array1));
+		System.out.println("Массив 2: " + Arrays.toString(array2));
 
-        try {
-            int[] result = subtractingArrayElements(array1, array2);
+		try {
+			int[] result = subtractingArrayElements(array1, array2);
 
-            System.out.println("\nМассив разности элементов двух массивов: " + Arrays.toString(result));
-        } catch (IllegalArgumentException e) {
-            System.out.println("\nПоймано исключение IllegalArgumentException: " + e.getMessage());
-        }
-    }
+			System.out.println("\nМассив разности элементов двух массивов: " + Arrays.toString(result));
+		} catch (IllegalArgumentException e) {
+			System.out.println("\nПоймано исключение IllegalArgumentException: " + e.getMessage());
+		}
+	}
 
-    // Генерация случайного массива
-    public static int[] generateRandomArray() {
-        Random random = new Random();
+	// Генерация случайного массива
+	public static int[] generateRandomArray() {
+		Random random = new Random();
 
-        int length = random.nextInt(5) + 1;
-        int[] array = new int[length];
+		int length = random.nextInt(5) + 1;
+		int[] array = new int[length];
 
-        for (int i = 0; i < length; i++) {
-            array[i] = random.nextInt(51); 
-        }
-        return array;
-    }
+		for (int i = 0; i < length; i++) {
+			array[i] = random.nextInt(51);
+		}
+		return array;
+	}
 
-    // Вычисление разности элементов двух массивов
-    public static int[] subtractingArrayElements(int[] array1, int[] array2) {
-        if (array1.length != array2.length) {
-            throw new IllegalArgumentException("Длины массивов не равны");
-        }
+	// Вычисление разности элементов двух массивов
+	public static int[] subtractingArrayElements(int[] array1, int[] array2) {
+		if (array1.length != array2.length) {
+			throw new IllegalArgumentException("Длины массивов не равны");
+		}
 
-        int[] result = new int[array1.length];
-        for (int i = 0; i < array1.length; i++) {
-            result[i] = array1[i] - array2[i];
-        }
+		int[] result = new int[array1.length];
+		for (int i = 0; i < array1.length; i++) {
+			result[i] = array1[i] - array2[i];
+		}
 
-        return result;
-    }
+		return result;
+	}
 }
-
