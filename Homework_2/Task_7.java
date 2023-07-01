1b29319baa5873eee5e331b078f4e06a1b2eaa3a
FoxA23 <afoxaa96@gmail.com>
2023-07-01 15:18:30 +0300

    Добавление файлов 2ого семинара:
    - Task_5.java
    - Task_6.java
    - Task_7.java

---
+++ Homework_2/Task_7.java
@@ -0,0 +1,23 @@
+package Homework_2;
+
+import java.util.Scanner;
+
+public class Task_7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in, "CP866");
+
+        System.out.print("Введите строку: ");
+        String input = scanner.nextLine();
+
+        try {
+            if (input.isEmpty()) {
+                throw new Exception("Пустые строки вводить нельзя!!!");
+            } else {
+                System.out.println("Вы ввели: " + input);
+            }
+        } catch (Exception e) {
+            System.out.println("Ошибка ввода: " + e.getMessage());
+        }
+    }
+}
+
