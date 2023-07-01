4d0a18c72b7a88826dff701e67393ed2ec8f1ac8
FoxA23 <afoxaa96@gmail.com>
2023-07-01 15:15:48 +0300

    Добавление решения задач 2 семинара:
    - Task_4.java
    - Task_5.java
    - Task_6.java
    - Task_7.java

--- Homework_2/Task_4.java
+++ Homework_2/Task_4.java
@@ -1,5 +1,32 @@
 package Homework_2;
 
+import java.util.Scanner;
+
 public class Task_4 {
-	
+    public static void main(String[] args) {
+        float number = inputOfFloatNumber();
+        System.out.println("Вы ввели число: " + number);
+    }
+
+    public static float inputOfFloatNumber() {
+        Scanner scanner = new Scanner(System.in);
+        float number = 0;
+        boolean validInput = false;
+
+        while (!validInput) {
+            System.out.print("\nВведите дробное число(разделитель ','): ");
+
+            if (scanner.hasNextFloat()) {
+                number = scanner.nextFloat();
+                validInput = true;
+            } else {
+                System.out.println("Некорректный ввод числа, попробуйте еще раз!");
+                scanner.nextLine(); 
+            }
+        }
+
+        return number;
+    }
 }
+
+
